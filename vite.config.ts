a553edfe88b54a169527f99f23e4bab266ce3769
Toshal <43895919+Maspie@users.noreply.github.com>
2025-04-05 21:53:10 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-const repoName = "toshal-portfolio";
-
 export default defineConfig({
-  base: `/${repoName}/`,
+  base: "/toshal-portfolio/", // 👈 this is important
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "client", "src"),
+      "@": path.resolve(__dirname, "client/src"),
     },
   },
-  root: path.resolve(__dirname, "client"),
+  root: "client",
   build: {
     outDir: path.resolve(__dirname, "dist"),
     emptyOutDir: true,
