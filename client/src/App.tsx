83dbf4068262ea135c01dc6f027e87cfca46f96b
Toshal <43895919+Maspie@users.noreply.github.com>
2025-04-05 22:01:03 -0700

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,19 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import Home from "@/pages/Home";
 import { ThemeProvider } from "./lib/theme";
 
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={Home} />
-    </Switch>
-  );
-}
-
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
-        <Router />
+        <Router base="/toshal-portfolio">
+          <Switch>
+            <Route path="/" component={Home} />
+          </Switch>
+        </Router>
         <Toaster />
       </ThemeProvider>
     </QueryClientProvider>
